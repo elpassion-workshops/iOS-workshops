fcd3eaf2588a1e8d8ba02780fae0adfad062ae12
Dariusz Rybicki <darrarski@gmail.com>
2016-09-09 22:18:55 +0200

    Add subviews to PokemonViewController's view

--- Workshops/PokemonViewController.swift
+++ Workshops/PokemonViewController.swift
@@ -23,6 +23,15 @@ class PokemonViewController: UIViewController {
     override func loadView() {
         view = UIView(frame: CGRect.zero)
         view.backgroundColor = UIColor.whiteColor()
+        
+        let imageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 300, height: 300))
+        view.addSubview(imageView)
+        
+        let nameLabel = UILabel(frame: CGRect(x: 10, y: 320, width: 300, height: 30))
+        view.addSubview(nameLabel)
+        
+        let detailsLabel = UILabel(frame: CGRect(x: 10, y: 360, width: 300, height: 30))
+        view.addSubview(detailsLabel)
     }
     
 }
