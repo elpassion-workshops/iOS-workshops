f9bde77a60d813262b92809670300518871f5075
Marcin Wieclawski <marc.wieclawski@gmail.com>
2016-09-09 21:15:20 +0200

    Update PokemonsListViewController to present list of available pokemons

--- Workshops/PokemonsListViewController.swift
+++ Workshops/PokemonsListViewController.swift
@@ -13,11 +13,24 @@ class PokemonsListViewController: UITableViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
     }
     
     required init?(coder aDecoder: NSCoder) { fatalError("Not implemented") }
+}
 
+extension PokemonsListViewController {
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Pokemons.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cellIdentifier", forIndexPath: indexPath)
+        let pokemon = Pokemons[indexPath.row]
+        cell.textLabel?.text = pokemon.name
+        return cell
+    }
 }
 
 struct Pokemon {
