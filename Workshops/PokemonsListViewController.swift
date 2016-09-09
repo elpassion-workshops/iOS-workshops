50efe1f4ba2d10d706b67629420405454e1b0e19
Marcin Wieclawski <marc.wieclawski@gmail.com>
2016-09-09 13:23:00 +0200

    Create a new window of size appropriate for current device. Renamed ViewController to PokemonsListViewController

--- Workshops/PokemonsListViewController.swift
+++ Workshops/PokemonsListViewController.swift
@@ -1,19 +1,22 @@
 //
-//  ViewController.swift
-//  Workshops
-//
 //  Created by Marcin Wieclawski on 09/09/16.
 //  Copyright © 2016 El Passion. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class PokemonsListViewController: UITableViewController {
 
+    init() {
+        super.init(style: .Plain)
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    required init?(coder aDecoder: NSCoder) { fatalError("Not implemented") }
 
 }
 
