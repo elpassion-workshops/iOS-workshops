34a567091d5bb2c9570c324cdd3ce16b02c5fa2f
Dariusz Rybicki <darrarski@gmail.com>
2016-09-09 22:36:02 +0200

    Setup auto layout constraints in PokemonViewController

--- Workshops/PokemonViewController.swift
+++ Workshops/PokemonViewController.swift
@@ -24,18 +24,52 @@ class PokemonViewController: UIViewController {
         view = UIView(frame: CGRect.zero)
         view.backgroundColor = UIColor.whiteColor()
         
-        let imageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 300, height: 300))
+        let imageView = UIImageView(frame: CGRect.zero)
         view.addSubview(imageView)
         
-        let nameLabel = UILabel(frame: CGRect(x: 10, y: 320, width: 300, height: 30))
+        let nameLabel = UILabel(frame: CGRect.zero)
         view.addSubview(nameLabel)
         
-        let detailsLabel = UILabel(frame: CGRect(x: 10, y: 360, width: 300, height: 30))
+        let detailsLabel = UILabel(frame: CGRect.zero)
         view.addSubview(detailsLabel)
         
         imageView.image = pokemon.image.toUIImage()
         nameLabel.text = pokemon.name
         detailsLabel.text = "Attack: \(pokemon.attack) Defence: \(pokemon.defense) Weight: \(pokemon.weight)"
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        detailsLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let imageViewTopConstraint = imageView.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 10)
+        imageViewTopConstraint.active = true
+        
+        let imageViewLeftConstraint = imageView.leftAnchor.constraintEqualToAnchor(imageView.superview?.leftAnchor, constant: 10)
+        imageViewLeftConstraint.active = true
+        
+        let imageViewRightConstraint = imageView.rightAnchor.constraintEqualToAnchor(imageView.superview?.rightAnchor, constant: -10)
+        imageViewRightConstraint.active = true
+        
+        let imageViewHeightConstraint = imageView.heightAnchor.constraintEqualToAnchor(imageView.widthAnchor)
+        imageViewHeightConstraint.active = true
+        
+        let nameLabelTopConstraint = nameLabel.topAnchor.constraintEqualToAnchor(imageView.bottomAnchor, constant: 10)
+        nameLabelTopConstraint.active = true
+        
+        let nameLabelLeftConstraint = nameLabel.leftAnchor.constraintEqualToAnchor(nameLabel.superview?.leftAnchor, constant: 10)
+        nameLabelLeftConstraint.active = true
+        
+        let nameLabelRightConstraint = nameLabel.rightAnchor.constraintEqualToAnchor(nameLabel.superview?.rightAnchor, constant: -10)
+        nameLabelRightConstraint.active = true
+        
+        let detailsLabelTopConstraint = detailsLabel.topAnchor.constraintEqualToAnchor(nameLabel.bottomAnchor, constant: 10)
+        detailsLabelTopConstraint.active = true
+        
+        let detailsLabelLeftConstraint = detailsLabel.leftAnchor.constraintEqualToAnchor(detailsLabel.superview?.leftAnchor, constant: 10)
+        detailsLabelLeftConstraint.active = true
+        
+        let detailsLabelRightConstraint = detailsLabel.rightAnchor.constraintEqualToAnchor(detailsLabel.superview?.rightAnchor, constant: -10)
+        detailsLabelRightConstraint.active = true
     }
     
 }
