c42a0b0355dd3765af0030787f676abd4d9b6e89
Dariusz Rybicki <darrarski@gmail.com>
2016-09-11 13:03:04 +0200

    Use SnapKit in PokemonViewController

--- Workshops/PokemonViewController.swift
+++ Workshops/PokemonViewController.swift
@@ -4,12 +4,11 @@
 //
 
 import UIKit
+import SnapKit
 
 class PokemonViewController: UIViewController {
     
     let pokemon: Pokemon
-    private var portraitConstraints = [NSLayoutConstraint]()
-    private var landscapeConstraints = [NSLayoutConstraint]()
     
     init(pokemon: Pokemon) {
         self.pokemon = pokemon
@@ -22,67 +21,74 @@ class PokemonViewController: UIViewController {
         fatalError("Not implemented")
     }
     
+    let imageView = UIImageView(frame: CGRect.zero)
+    let nameLabel = UILabel(frame: CGRect.zero)
+    let detailsLabel = UILabel(frame: CGRect.zero)
+    
     override func loadView() {
         view = UIView(frame: CGRect.zero)
         view.backgroundColor = UIColor.whiteColor()
-        
-        let imageView = UIImageView(frame: CGRect.zero)
         view.addSubview(imageView)
-        
-        let nameLabel = UILabel(frame: CGRect.zero)
         view.addSubview(nameLabel)
-        
-        let detailsLabel = UILabel(frame: CGRect.zero)
         view.addSubview(detailsLabel)
         
         imageView.image = pokemon.image.toUIImage()
         nameLabel.text = pokemon.name
         detailsLabel.text = "Attack: \(pokemon.attack) Defence: \(pokemon.defense) Weight: \(pokemon.weight)"
-        
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        detailsLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        portraitConstraints = [
-            imageView.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 10),
-            imageView.leftAnchor.constraintEqualToAnchor(imageView.superview?.leftAnchor, constant: 10),
-            imageView.rightAnchor.constraintEqualToAnchor(imageView.superview?.rightAnchor, constant: -10),
-            imageView.heightAnchor.constraintEqualToAnchor(imageView.superview?.heightAnchor, multiplier: 0.5, constant: -20),
-            
-            nameLabel.topAnchor.constraintEqualToAnchor(imageView.bottomAnchor, constant: 10),
-            nameLabel.leftAnchor.constraintEqualToAnchor(nameLabel.superview?.leftAnchor, constant: 10),
-            nameLabel.rightAnchor.constraintEqualToAnchor(nameLabel.superview?.rightAnchor, constant: -10),
-            
-            detailsLabel.topAnchor.constraintEqualToAnchor(nameLabel.bottomAnchor, constant: 10),
-            detailsLabel.leftAnchor.constraintEqualToAnchor(detailsLabel.superview?.leftAnchor, constant: 10),
-            detailsLabel.rightAnchor.constraintEqualToAnchor(detailsLabel.superview?.rightAnchor, constant: -10)
-        ]
-        
-        landscapeConstraints = [
-            imageView.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 10),
-            imageView.leftAnchor.constraintEqualToAnchor(imageView.superview?.leftAnchor, constant: 10),
-            imageView.bottomAnchor.constraintEqualToAnchor(bottomLayoutGuide.topAnchor, constant: -10),
-            imageView.widthAnchor.constraintEqualToAnchor(imageView.superview?.widthAnchor, multiplier: 0.5, constant: -20),
-            
-            nameLabel.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 10),
-            nameLabel.leftAnchor.constraintEqualToAnchor(imageView.rightAnchor, constant: 10),
-            nameLabel.rightAnchor.constraintEqualToAnchor(nameLabel.superview?.rightAnchor, constant: -10),
-            
-            detailsLabel.topAnchor.constraintEqualToAnchor(nameLabel.bottomAnchor, constant: 10),
-            detailsLabel.leftAnchor.constraintEqualToAnchor(imageView.rightAnchor, constant: 10),
-            detailsLabel.rightAnchor.constraintEqualToAnchor(nameLabel.superview?.rightAnchor, constant: -10)
-        ]
+        detailsLabel.numberOfLines = 0
     }
     
     override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         
+        imageView.snp_removeConstraints()
+        nameLabel.snp_removeConstraints()
+        detailsLabel.snp_removeConstraints()
+        
         if traitCollection.horizontalSizeClass == .Compact && traitCollection.verticalSizeClass == .Regular {
-            NSLayoutConstraint.deactivateConstraints(landscapeConstraints)
-            NSLayoutConstraint.activateConstraints(portraitConstraints)
-        } else {
-            NSLayoutConstraint.deactivateConstraints(portraitConstraints)
-            NSLayoutConstraint.activateConstraints(landscapeConstraints)
+            
+            imageView.snp_makeConstraints { make in
+                make.top.equalTo(snp_topLayoutGuideBottom).offset(10)
+                make.left.equalTo(10)
+                make.right.equalTo(-10)
+                make.height.equalTo(imageView.superview!.snp_height)
+                    .multipliedBy(0.5).offset(-20)
+            }
+            
+            nameLabel.snp_makeConstraints { (make) in
+                make.top.equalTo(imageView.snp_bottom).offset(10)
+                make.left.equalTo(10)
+                make.right.equalTo(-10)
+            }
+            
+            detailsLabel.snp_makeConstraints { (make) in
+                make.top.equalTo(nameLabel.snp_bottom).offset(10)
+                make.left.equalTo(10)
+                make.right.equalTo(-10)
+            }
+            
+        }
+        else {
+        
+            imageView.snp_makeConstraints {
+                $0.top.equalTo(snp_topLayoutGuideBottom).offset(10)
+                $0.bottom.equalTo(snp_bottomLayoutGuideTop).offset(-10)
+                $0.left.equalTo(10)
+                $0.width.equalTo(imageView.superview!.snp_width).multipliedBy(0.5).offset(-20)
+            }
+            
+            nameLabel.snp_makeConstraints { (make) in
+                make.top.equalTo(snp_topLayoutGuideBottom).offset(10)
+                make.left.equalTo(imageView.snp_right).offset(10)
+                make.right.equalTo(-10)
+            }
+            
+            detailsLabel.snp_makeConstraints { (make) in
+                make.top.equalTo(nameLabel.snp_bottom).offset(10)
+                make.left.equalTo(imageView.snp_right).offset(10)
+                make.right.equalTo(-10)
+            }
+            
         }
     }
     
